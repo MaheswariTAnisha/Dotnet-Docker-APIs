9e6b436cda34d1decc5332df9685335b54e2e17c
MaheswariTAnisha <133736235+MaheswariTAnisha@users.noreply.github.com>
2025-02-26 15:52:16 +0530

    Update Program.cs

--- MyApiProject/Program.cs
+++ MyApiProject/Program.cs
@@ -29,7 +29,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/app1/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
